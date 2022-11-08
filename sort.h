d6ffc75f8238ddc618b2b3e9f7492d8145f437b1
root <Olaleyebode001@gmail.com>
2022-11-08 11:55:31 -0800

    Header file

--- sort.h
+++ sort.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <unistd.h>
 
 /* Comparison direction macros for bitonic sort */
 #define UP 0
@@ -33,11 +36,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Printing helper functions */
-void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 
-/* Sorting algoritms */
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -51,4 +53,4 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-#endif /* SORT_H */
+#endif 
